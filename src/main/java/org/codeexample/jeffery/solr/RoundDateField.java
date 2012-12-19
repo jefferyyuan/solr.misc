93b99b5a436f29d46ebaf6a6c90c61649ac9a9b9
Jeffery Yuan <yuanyun.cn@gmail.com>
2012-12-19 18:54:33 -0500

    Smart RoundDateField

--- src/main/java/org/codeexample/jeffery/solr/RoundDateField.java
+++ src/main/java/org/codeexample/jeffery/solr/RoundDateField.java
@@ -14,60 +14,77 @@ import org.apache.solr.schema.SchemaField;
 import org.apache.solr.schema.TrieDateField;
 
 public class RoundDateField extends TrieDateField {
-
-	/**
-	 * roundTo can be predefined format, such as day, second, or it can be a
-	 * format string, such as yyyy-MM-dd'T'hh:00:00.0'Z'
-	 */
-	private String roundTo = "roundTo";
-	private static String ROUND_TO_DAY = "DAY";
-	private static String FORMAT_DAY = "yyyy-MM-dd'T'00:00:00.0'Z'";
-
-	// yyyy-MM-dd'T'HH:mm:ss.SSS'Z'
-	private static String ROUND_SECOND = "SECOND";
-	private static String FORMAT_SECOND = "yyyy-MM-dd'T'HH:mm:ss'Z'";
-
-	@Override
-	protected void init(IndexSchema schema, Map<String, String> args) {
-		if (args != null) {
-			roundTo = args.remove(roundTo);
-		}
-		super.init(schema, args);
-	}
-
-	@Override
-	public IndexableField createField(SchemaField field, Object value,
-			float boost) {
-
-		try {
-			// Value is a string: yyyy-MM-dd'T'HH:mm:ss'Z'
-			Date date = parseSolrDate(value.toString());
-			DateFormat df = null;
-
-			if (ROUND_TO_DAY.equalsIgnoreCase(roundTo)) {
-				df = new SimpleDateFormat(FORMAT_DAY);
-			} else if (ROUND_SECOND.equalsIgnoreCase(roundTo)) {
-				df = new SimpleDateFormat(FORMAT_SECOND);
-			} else {
-				// roundTo can also be format such as yyyy-MM-dd'T'hh:00:00.0'Z'
-				df = new SimpleDateFormat(roundTo);
-			}
-			if (df != null) {
-				value = df.format(date);
-			}
-
-		} catch (Exception ex) {
-			throw new RuntimeException(ex);
-		}
-
-		return super.createField(field, value, boost);
-	}
-
-	public Date parseSolrDate(String dateString) throws ParseException {
-		SimpleDateFormat sdf = new SimpleDateFormat(
-				"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US);
-		sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
-
-		return sdf.parse(dateString);
-	}
+	  
+	  private SimpleDateFormat sdf;
+	  
+	  private String fromFormat = null;
+	  private static String PARAM_FROM_FORMAT = "fromFormat";
+	  private static String DATE_FORMAT_UNIX_SECOND = "UNIX_SECOND";
+	  private static String DATE_FORMAT_UNIX_MILLSECOND = "UNIX_MILLSECOND";
+	  
+	  private static long MS_IN_DAY = 3600 * 24 * 1000;
+	  private static final long SECONDS_FROM_EPCO = new Date().getTime() / 1000;
+	  
+	  @Override
+	  protected void init(IndexSchema schema, Map<String,String> args) {
+	    if (args != null) {
+	      fromFormat = args.remove(PARAM_FROM_FORMAT);
+	    }
+	    sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US);
+	    sdf.setTimeZone(UTC);
+	    super.init(schema, args);
+	  }
+	  
+	  /**
+	   * if value > SECONDS_FROM_EPCO, then treat value as milliseconds, otherwise
+	   * treat value as seconds
+	   * 
+	   * @param value
+	   * @return
+	   */
+	  private long convertToMillseconds(long value) {
+	    long result = value;
+	    if (value < SECONDS_FROM_EPCO) {
+	      result = result * 1000L;
+	    }
+	    return result;
+	  }
+	  
+	  @Override
+	  public IndexableField createField(SchemaField field, Object value, float boost) {
+	    
+	    try {
+	      long millseconds = -1;
+	      
+	      try {
+	        millseconds = Long.parseLong(value.toString());
+	        
+	        if (fromFormat != null) {
+	          if (DATE_FORMAT_UNIX_MILLSECOND.equalsIgnoreCase(fromFormat)) {
+	            // do nothing
+	          } else if (DATE_FORMAT_UNIX_SECOND.equalsIgnoreCase(fromFormat)) {
+	            millseconds = millseconds * 1000L;
+	          } else {
+	            throw new RuntimeException("Invalid fromFormat: " + fromFormat);
+	          }
+	        } else {
+	          millseconds = convertToMillseconds(millseconds);
+	        }
+	        
+	      } catch (Exception ex) {
+	        // so it should be a date string
+	        millseconds = sdf.parse(value.toString()).getTime();
+	      }
+	      
+	      millseconds = (millseconds / MS_IN_DAY) * MS_IN_DAY + (MS_IN_DAY / 2);
+	      
+	      value = new Date(millseconds);
+	      
+	      // returned value must be a date time string
+	    } catch (Exception ex) {
+	      throw new RuntimeException(ex);
+	    }
+	    
+	    return super.createField(field, value, boost);
+	  }
 }
